8226bcb18096ac7a91dfeb88af87d6f239e9d495
David Runnels <david.runnels@gmail.com>
2015-02-06 14:54:07 -0600

    add first and last name

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -9,6 +9,8 @@ class UserTableSeeder extends Seeder {
         $user = new User();
         $user->email = $_ENV['DEFAULT_USER_EMAIL'];
         $user->password = $_ENV['DEFAULT_USER_PASSWORD'];
+        $user->last_name = $_ENV['DEFAULT_USER_LAST_NAME'];
+        $user->first_name = $_ENV['DEFAULT_USER_FIRST_NAME'];
         $user->save();
     }
 
