a3782ff95d7d103dbaf05dfcedbef889ca7dc2b6
David Runnels <david.runnels@gmail.com>
2015-02-06 11:37:49 -0600

    refactored img

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.master')
 
 @section('content')
-	<img src="img/drwebdev.jpg" alt="david runnels web dev" id="drmain" height="800" width="900">
+	<img src="img/drwebdev_sm.jpg" alt="david runnels web dev" id="drmain" height="800" width="900">
 @stop
